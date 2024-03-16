e6a537db59b2a5c42349d978cd1bcd1d1245fca3
jeongwon <lmsjava2@gmail.com>
2024-03-16 17:52:36 +0900

    first commit

---
+++ src/test/java/com/example/restmsa/RestMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
